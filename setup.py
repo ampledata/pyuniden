c38335e8f74d711c71c50d11f3c089019d75a0ef
Greg Albrecht <gba@onbeep.com>
2015-09-29 15:08:49 -0700

    rewrote setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,19 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from distutils.core import setup
-setup(name='scanner',
-      version='1.0',
-      packages=['scanner'],
-      )
+
+import setuptools
+
+
+setuptools.setup(
+    name='uniden_api',
+    version=render_version(),
+    description='Uniden Scanner API',
+    zip_safe=False,
+    packages=['uniden_api'],
+    setup_requires=['coverage==3.7.1', 'nose>=1.3.1'],
+    install_requires=[
+        'pyyaml',
+        'pyserial'
+    ]
+)
